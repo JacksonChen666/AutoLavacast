962785a17c0df5eebac736d9f4a3d0c6b610f316
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-12-18 01:53:56 +0100

    minor unaffected changes

--- src/main/kotlin/com/jacksonchen666/autolavacast/processor/WaterPlace.kt
+++ src/main/kotlin/com/jacksonchen666/autolavacast/processor/WaterPlace.kt
@@ -5,7 +5,7 @@ import org.bukkit.block.Block
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
-class WaterPlace(private val watchBlock: Block, private val placeBlock: Block, val plugin: JavaPlugin) : BukkitRunnable() {
+class WaterPlace(private val watchBlock: Block, private val placeBlock: Block, private val plugin: JavaPlugin) : BukkitRunnable() {
     override fun run() {
         if (watchBlock.type == Material.LAVA) {
             placeBlock.type = Material.WATER
